243e736c1752c93fdd8f16f491138940be78ab68
obeidanoorhamdan-png <obeidanoorhamdan@gmail.com>
2026-01-27 10:22:40 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -12,8 +12,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQu
 from flask import Flask
 
 # --- الإعدادات ---
-TOKEN = os.environ.get('TOKEN', "7324911542:AAFqB9NRegwE2_bG5rCTaEWocbh8N3vgWeo")
-MISTRAL_KEY = os.environ.get('MISTRAL_KEY', "EABRT5zGsHYhezkaJJomt15VR2iBrPWq")
+TOKEN = os.environ.get('TOKEN', "7324911542:AAGcVkwzjtf3wDB3u7cprOLVyoMLA5JCm8U")
+MISTRAL_KEY = os.environ.get('MISTRAL_KEY', "KaPHLZHUoimtxAAb4sOxUQYRspqFktCz")
 MISTRAL_URL = "https://api.mistral.ai/v1/chat/completions"
 DB_NAME = "abood-gpt.db"
 
